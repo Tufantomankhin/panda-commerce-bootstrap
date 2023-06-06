d18cc789a61680f16a658e146ee07df942b8fffd
Tufanto Mankhin <tufantomankhin@Tufanto-Mankhin-Portonics.local>
2023-06-06 20:20:23 +0600

    button color changeed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-console.log("it's working");
+document.getElementById('btn-supply').style.backgroundColor = 'orange'
+
+const buttons350 = document.querySelectorAll('.supply-350')
+for (const button350 of buttons350) {
+    button350.style.borderRadius = '10px'
+    button350.style.color = 'white'
+}
