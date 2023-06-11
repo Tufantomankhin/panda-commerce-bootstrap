8cb7e018e02ee35034a39ca95eee1322eff91c19
Tufanto Mankhin <tufantomankhin@Tufanto-Mankhin-Portonics.local>
2023-06-11 18:16:17 +0600

    js style btn width added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-document.getElementById('btn-supply').style.backgroundColor = 'orange'
+// document.getElementById('btn-supply').style.backgroundColor = 'orange'
 
 const buttons350 = document.querySelectorAll('.supply-350')
 for (const button350 of buttons350) {
+    button350.style.backgroundColor = 'tomato'
     button350.style.borderRadius = '10px'
     button350.style.color = 'white'
+    button350.style.width = '120px'
+    button350.style.hight = '120px'
 }
