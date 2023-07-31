83333d01d09e724c155843856ab4aef849376fde
Tufanto Mankhin <tufantomankhin@Tufanto-Mankhin-Portonics.local>
2023-07-31 11:17:54 +0600

    adding heigt

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
-document.getElementById('btn-supply').addEventListener('click', function () {
+document.getElementById('btn-supply').addEventListener('click', function (event) {
         console.log('buttton clicked');
-        // function myFunction(event) {
-        //     if (document.getElementById('btn-supply').disabled = true) {
-        //         event.removeAtribute('disabled');
-        //     } 
-        //     else { document.getElementById('btn-supply').disabled = false;
-        //         event.setAtribute('disabled');
-        //     }
-        // }
-    })
+            if (document.getElementById('btn-supply').disabled = true) {
+                event.removeAtribute('disabled');
+            } 
+            else { document.getElementById('btn-supply').disabled = false;
+                event.setAtribute('disabled');
+            }
+        }
+    )
 
 const buttons350 = document.querySelectorAll('.supply-350')
 for (const button350 of buttons350) {
@@ -16,5 +15,6 @@ for (const button350 of buttons350) {
     button350.style.borderRadius = '10px'
     button350.style.color = 'white'
     button350.style.width = '120px'
-    button350.style.hight = '120px'
+    button350.style.height = '40px'
+
 }
